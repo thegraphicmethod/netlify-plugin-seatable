0536aa9af8c6a4b240817d99f2efc1a53e75da6a
Sergio Galán <sergio@eclectico.net>
2023-06-03 02:15:19 +0200

    multiple variables

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 Netlify Build plugin seatable - Downloads and save a seatable database as json file.
+By runing this plugin a json file will be downloaded from a seatable database and saved in the specified folder.
+
+Please notice that this plugin is not officialy supported by seatable.
+
+The json file is saved before building the site so it can be used in the build process for instance when generating a static site with NUXT.
+The json file is not commited to the repository, it is only available during the build process.
+
 
 # Install
 
-Please install this plugin from the Netlify app.
+Please install this plugin from the Netlify app or the netlify.toml file.
 
 # Configuration
 
 The following `inputs` options are available.
+SEATABLE_TABLE_NAME which is the name of the table to download. If you want to download multiple tables simply write the names separated by a comma (no spaces betwen names)
+DATA_FOLDER which is the folder where the json file will be saved.
+
+***IMPORTANT***
+Besides the inputs in the netlify.toml file it is **mandatory** to include the following environment variables in the netlify app:
+* SEATABLE_APP_TOKEN which is the token of the seatable app. You can get the api Key for a Base in the advanced section of the base settings.
+
+This is my first netlify plugin so any feedback is welcome.
--- src/index.js
+++ src/index.js
@@ -16,27 +16,38 @@ module.exports = {
   onPreBuild: async ({ utils, inputs }) => {
     const tableName = inputs.SEATABLE_TABLE_NAME
     const baseFolder = inputs.DATA_FOLDER
+    let tableNames = tableName.split(',')
+    let basetoken, dtableUuid, server = ''
     // transform table name in an array split by comma
-    const p1 = await axios(config)
+    return axios(config)
       .then(function (response) {
-        const basetoken = response.data.access_token
-        const dtableUuid = response.data.dtable_uuid
-        const server = response.data.dtable_server
-        const config2 = {
-          method: 'get',
-          url: `${server}/api/v1/dtables/${dtableUuid}/rows/?table_name=${tableName}&view_name=Default View`,
-          headers: {
-            Authorization: 'Token ' + basetoken,
-          },
-        }
-        return config2
+        basetoken = response.data.access_token
+        dtableUuid = response.data.dtable_uuid
+        server = response.data.dtable_server   
+        let promises = []
+        tableNames.forEach((tableName) => {
+          promises.push(pullTableData(tableName, server, basetoken, dtableUuid,baseFolder, utils))
+        })    
+        return Promise.all(promises)
+        
       })
       .catch(function (error) {
         console.log(error)
         utils.build.cancelBuild('Error: ' + error)
       })
-    
-    const p2 = await axios(p1)
+  },
+}
+
+function pullTableData(tableName, server, basetoken, dtableUuid, baseFolder, utils) {
+  const config2 = {
+    method: 'get',
+    url: `${server}/api/v1/dtables/${dtableUuid}/rows/?table_name=${tableName}&view_name=Default View`,
+    headers: {
+      Authorization: 'Token ' + basetoken,
+    },
+  }
+
+  return axios(config2)
       .then(function (response) {
         fs.writeFile(
           `${baseFolder}/${tableName}.json`,
@@ -44,17 +55,14 @@ module.exports = {
           'utf8',
           function (err) {
             if (err) {
-              utils.build.cancelBuild('Error: writting file' + err)
+              utils.build.cancelBuild(`Error: writting file ${tableName}.json` + err)
             }
-            console.log('JSON file 1 has been saved.')
+            console.log(`JSON file ${tableName}.json has been saved.`)
           }
         )
-        console.log(response.data.rows.length + 'rows saved to file.')
+        console.log(`${response.data.rows.length} rows saved to file.  ${tableName}`)
       })
       .catch(function (error) {
         utils.build.cancelBuild('Error: ' + error)
-      })
-
-    return Promise.all([p2])
-  },
+      })  
 }
